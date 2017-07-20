61d27e30a24861627381ecc3433423adc36884f4
Ervin <ervin@Ervins-MacBook-Pro.local>
2017-07-20 16:40:52 +0300

    Format code

--- celladapter/src/main/java/io/erva/celladapter/CellAdapter.java
+++ celladapter/src/main/java/io/erva/celladapter/CellAdapter.java
@@ -17,97 +17,97 @@ import java.util.Map;
 
 public class CellAdapter extends RecyclerView.Adapter<Cell> {
 
-	private final Map<Class, Class<? extends Cell>> itemCellMap = new ArrayMap<>();
-	private final List<Class> viewTypes = new ArrayList<>();
-	private final SparseArray<Cell.Listener<?>> typeListenerMapping = new SparseArray<>();
-	private List items = new ArrayList<>();
+    private final Map<Class, Class<? extends Cell>> itemCellMap = new ArrayMap<>();
+    private final List<Class> viewTypes = new ArrayList<>();
+    private final SparseArray<Cell.Listener<?>> typeListenerMapping = new SparseArray<>();
+    private List items = new ArrayList<>();
 
-	public void registerCell(@NonNull Class<?> itemClass,
-							 @NonNull Class<? extends Cell> cellClass) {
-		registerCell(itemClass, cellClass, null);
-	}
+    public void registerCell(@NonNull Class<?> itemClass,
+                             @NonNull Class<? extends Cell> cellClass) {
+        registerCell(itemClass, cellClass, null);
+    }
 
-	public void registerCell(@NonNull Class<?> itemClass,
-							 @NonNull Class<? extends Cell> cellClass,
-							 @Nullable Cell.Listener<?> cellListener) {
-		itemCellMap.put(itemClass, cellClass);
-		int type = viewTypes.indexOf(itemClass);
-		if (type == -1) viewTypes.add(itemClass);
-		if (cellListener != null) registerListener(itemClass, cellListener);
-	}
+    public void registerCell(@NonNull Class<?> itemClass,
+                             @NonNull Class<? extends Cell> cellClass,
+                             @Nullable Cell.Listener<?> cellListener) {
+        itemCellMap.put(itemClass, cellClass);
+        int type = viewTypes.indexOf(itemClass);
+        if (type == -1) viewTypes.add(itemClass);
+        if (cellListener != null) registerListener(itemClass, cellListener);
+    }
 
-	private void registerListener(Class<?> itemClass,
-								  @Nullable Cell.Listener<?> cellListener) {
-		int index = viewTypes.indexOf(itemClass);
-		if (index < 0)
-			throw new IllegalStateException(itemClass.getSimpleName() + " is not registered as Cell");
-		typeListenerMapping.put(index, cellListener);
-	}
+    private void registerListener(Class<?> itemClass,
+                                  @Nullable Cell.Listener<?> cellListener) {
+        int index = viewTypes.indexOf(itemClass);
+        if (index < 0)
+            throw new IllegalStateException(itemClass.getSimpleName() + " is not registered as Cell");
+        typeListenerMapping.put(index, cellListener);
+    }
 
-	@Override
-	public Cell onCreateViewHolder(ViewGroup parent, int viewType) {
-		Class<? extends Cell> cellClass = itemCellMap.get(viewTypes.get(viewType));
-		Cell cell = buildCell(cellClass, parent);
-		cell.setCellDelegate(typeListenerMapping.get(viewType));
-		return cell;
-	}
+    @Override
+    public Cell onCreateViewHolder(ViewGroup parent, int viewType) {
+        Class<? extends Cell> cellClass = itemCellMap.get(viewTypes.get(viewType));
+        Cell cell = buildCell(cellClass, parent);
+        cell.setCellDelegate(typeListenerMapping.get(viewType));
+        return cell;
+    }
 
-	private Cell buildCell(Class<? extends Cell> cellClass, ViewGroup parent) {
-		Layout layoutAnnotation = cellClass.getAnnotation(Layout.class);
-		View cellView = LayoutInflater.from(parent.getContext()).inflate(layoutAnnotation.value(), parent, false);
-		RecyclerView.ViewHolder cellObject = null;
-		try {
-			Constructor<? extends RecyclerView.ViewHolder> constructor = cellClass.getConstructor(View.class);
-			cellObject = constructor.newInstance(cellView);
-		} catch (Exception e) {
-			Log.e("CellAdapter", "Can't create cell: " + e.getMessage());
-		}
-		return (Cell) cellObject;
-	}
+    private Cell buildCell(Class<? extends Cell> cellClass, ViewGroup parent) {
+        Layout layoutAnnotation = cellClass.getAnnotation(Layout.class);
+        View cellView = LayoutInflater.from(parent.getContext()).inflate(layoutAnnotation.value(), parent, false);
+        RecyclerView.ViewHolder cellObject = null;
+        try {
+            Constructor<? extends RecyclerView.ViewHolder> constructor = cellClass.getConstructor(View.class);
+            cellObject = constructor.newInstance(cellView);
+        } catch (Exception e) {
+            Log.e("CellAdapter", "Can't create cell: " + e.getMessage());
+        }
+        return (Cell) cellObject;
+    }
 
-	@Override
-	public void onBindViewHolder(Cell cell, int position) {
-		cell.bindViewInternal(getItem(position));
-	}
+    @Override
+    public void onBindViewHolder(Cell cell, int position) {
+        cell.bindViewInternal(getItem(position));
+    }
 
-	@Override
-	public int getItemViewType(int position) {
-		Class itemClass = items.get(position).getClass();
-		int index = viewTypes.indexOf(itemClass);
-		if (index < 0) {
-			throw new IllegalArgumentException(itemClass.getSimpleName() + " is not registered");
-		}
-		return index;
-	}
+    @Override
+    public int getItemViewType(int position) {
+        Class itemClass = items.get(position).getClass();
+        int index = viewTypes.indexOf(itemClass);
+        if (index < 0) {
+            throw new IllegalArgumentException(itemClass.getSimpleName() + " is not registered");
+        }
+        return index;
+    }
 
-	@Override
-	public int getItemCount() {
-		return items.size();
-	}
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
 
-	public boolean contains(Object item) {
-		return items.contains(item);
-	}
+    public boolean contains(Object item) {
+        return items.contains(item);
+    }
 
-	public int indexOf(Object item) {
-		return items.indexOf(item);
-	}
+    public int indexOf(Object item) {
+        return items.indexOf(item);
+    }
 
     public void replaceItem(int position, Object item) {
         items.set(position, item);
     }
 
-	public Object getItem(int position) {
-		return items.get(position);
-	}
+    public Object getItem(int position) {
+        return items.get(position);
+    }
 
-	public List getItems() {
-		return items;
-	}
+    public List getItems() {
+        return items;
+    }
 
-	public void setItems(List items) {
-		this.items = items;
-	}
+    public void setItems(List items) {
+        this.items = items;
+    }
 
     public void addItem(int position, Object item) {
         items.add(position, item);
@@ -130,7 +130,7 @@ public class CellAdapter extends RecyclerView.Adapter<Cell> {
         items.remove(position);
     }
 
-	public void clear() {
-		items.clear();
-	}
+    public void clear() {
+        items.clear();
+    }
 }
